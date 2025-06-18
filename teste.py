f9416b7e66e83c67da7b5a3dfbb1f51a0aab9c37
FlaviouSs <flaviotubaroni@gmail.com>
2025-06-17 23:42:00 -0300

    Comentarios

--- teste.py
+++ teste.py
@@ -1,13 +1,17 @@
 import grafoslib
 
+# 1.    Primeiro, o usuário deve instânciar a classe Grafo()
 g = grafoslib.Grafo()
 
+# 2.    O arquivo de input deve ser processado
 g.processar_input("./input1.txt")
 
+# 3.    A representação do grafo a ser utilizada é escolhida
 g.gerar_lista_adjacencia()
 g.gerar_matriz_adjacencia()
 
 
+# 4.    A lógica desejada é implementada
 g.DFS_por_lista(1, "./output1.txt")
 g.DFS_por_matriz(1, "./output2.txt")
 g.BFS_por_lista(1,"./output3.txt")

a578f78371752cbf9652fbdb84b7fb6b1c4af4c4
FlaviouSs <flaviotubaroni@gmail.com>
2025-06-20 18:20:58 -0300

    estudo de caso script

--- teste.py
+++ teste.py
@@ -4,7 +4,7 @@ import grafoslib
 g = grafoslib.Grafo()
 
 # 2.    O arquivo de input deve ser processado
-g.processar_input("./input2.txt")
+g.processar_input("./input1.txt")
 
 # 3.    A representação do grafo a ser utilizada é escolhida
 g.gerar_lista_adjacencia()
@@ -17,11 +17,11 @@ g.DFS_por_matriz(1, "./output2.txt")
 g.BFS_por_lista(1,"./output3.txt")
 g.BFS_por_matriz(1, "./output4.txt")
 
-g.encontrar_componentes_conexos()
+#g.encontrar_componentes_conexos()
 
+# PARTE 2
+print()
+distancias, caminhos = g.menor_caminho(origem=1, destino=2)
 
-distancias, pais = g.menor_caminho(origem=1, destino=4)
-print()
-print()
 print(distancias)
-print(pais)
+print(caminhos)
